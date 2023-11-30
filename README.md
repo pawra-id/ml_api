6655122674bb2aeef385abf646472031fd231a6e
ab-azmi <azmi.kiva@gmail.com>
2023-11-30 11:36:26 +0700

    add docker file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# ml_api
+# ML API
+This is repository for machine learning backend. Using FastAPI and Tensorflow.
+
+## Requiremenets
+- Python >= 3.10
+
+## Install
+- Clone the repo
+- run ```pip install --no-cache-dir -r requirements.txt``` in terminal
+- run ```pip install tensorflow``` in terminal
+- Finally, run ```uvicorn app.main:app --reload --port 8000```
+- Access the app at [http://127.0.0.1:8000/docs](http://127.0.0.1:8000/docs)
--- app/services/preprocess.py
+++ app/services/preprocess.py
@@ -1,6 +1,7 @@
 import nltk
 import string
 import re
+import ast
 from nltk.tokenize import word_tokenize
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 nltk.download('punkt')
@@ -18,6 +19,21 @@ def tokenizingText(text):
   text = word_tokenize(text)
   return text
 
+def slang_word(text):
+  with open("app/model/slang_words.txt", "r") as slang_file:
+    slang_content = slang_file.read()
+    slang_words = ast.literal_eval(slang_content)
+
+  filtered = []
+  for txt in text:
+    if txt not in slang_words.keys():
+      filtered.append(txt)
+    if txt in slang_words.keys():
+      x = txt.replace(txt, slang_words[txt])
+      filtered.append(x)
+  text = filtered
+  return text
+
 def stemmingText(text):
   factory = StemmerFactory()
   stemmer = factory.create_stemmer()
@@ -30,10 +46,11 @@ def toSentence(list_words): # Convert list of words into sentence
 
 
 def data_preprocessing(text):
-  cleaned_text = remove_punctuation(text)
-  cleaned_text = case_folding(cleaned_text)
-  tokenized_text = tokenizingText(cleaned_text)
-  stemmed_text = stemmingText(tokenized_text)
-  preprocessed_text = toSentence(stemmed_text)
+  cleaned_text = [remove_punctuation(x) for x in text]
+  cleaned_text = [case_folding(x) for x in cleaned_text]
+  tokenized_text = [tokenizingText(x) for x in cleaned_text]
+  slang_words = [slang_word(x) for x in tokenized_text]
+  stemmed_text = [stemmingText(x) for x in slang_words]
+  preprocessed_text = [toSentence(x) for x in stemmed_text]
 
   return preprocessed_text
