d532fcc23eba4f0a74be05e0d3484ac04071cf13
ab-azmi <azmi.kiva@gmail.com>
2023-12-10 01:19:32 +0700

    add exception for empty activity

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 import numpy as np
 from typing import List
 from tensorflow.keras.models import load_model
@@ -23,6 +23,10 @@ async def root():
 
 @app.post("/predict")
 async def predict(text: List[str]):
+    #if text empty, return http exception
+    if len(text) == 0:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Activity is empty")
+    
     model = load_model('app/model/bidirectional_lstm.h5')
     preprocessed_text = data_preprocessing(text)
     padded_sequences = tokenize(preprocessed_text)
